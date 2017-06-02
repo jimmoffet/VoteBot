f3f57af99ad2ed7fd9d42463b2769a2485897aca
jimmoffet <jimmoffet@gmail.com>
2017-06-02 15:50:14 -0400

    add /monkey route from twilio python quickstart guide doc

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, request, redirect, jsonify
+from twilio.twiml.messaging_response import MessagingResponse
 from scrapetest import scrape
 
 app = Flask(__name__)
@@ -11,6 +12,13 @@ def hello():
 def serve_schedule():
 	schedule = scrape('http://cambridgema.iqm2.com/Citizens/Detail_LegalNotice.aspx?ID=1008')
 	return jsonify(schedule)
+
+@app.route("/monkey", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+
+    resp = MessagingResponse().message("Hello, Mobile Monkey")
+    return str(resp)
 
 if __name__ == "__main__":
 	app.run(debug=False)
