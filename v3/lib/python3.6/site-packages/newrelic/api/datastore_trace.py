2e996bf9e2849609dd64c3a8a496283995888dbf
jimmoffet <jimmoffet@gmail.com>
2017-06-06 11:39:24 -0400

    add new relic monitoring python agent to requirements

---
+++ v3/lib/python3.6/site-packages/newrelic/api/datastore_trace.py
@@ -0,0 +1,85 @@
+import functools
+
+from newrelic.api.time_trace import TimeTrace
+from newrelic.api.transaction import current_transaction
+from newrelic.common.object_wrapper import FunctionWrapper, wrap_object
+from newrelic.core.datastore_node import DatastoreNode
+
+
+class DatastoreTrace(TimeTrace):
+
+    node = DatastoreNode
+
+    def __init__(self, transaction, product, target, operation,
+            host=None, port_path_or_id=None, database_name=None):
+
+        super(DatastoreTrace, self).__init__(transaction)
+
+        if transaction:
+            self.product = transaction._intern_string(product)
+            self.target = transaction._intern_string(target)
+            self.operation = transaction._intern_string(operation)
+        else:
+            self.product = product
+            self.target = target
+            self.operation = operation
+
+        self.host = host
+        self.port_path_or_id = port_path_or_id
+        self.database_name = database_name
+
+    def __repr__(self):
+        return '<%s %s>' % (self.__class__.__name__, dict(
+                product=self.product, target=self.target,
+                operation=self.operation))
+
+    def terminal_node(self):
+        return True
+
+
+def DatastoreTraceWrapper(wrapped, product, target, operation):
+
+    def _nr_datastore_trace_wrapper_(wrapped, instance, args, kwargs):
+        transaction = current_transaction()
+
+        if transaction is None:
+            return wrapped(*args, **kwargs)
+
+        if callable(product):
+            if instance is not None:
+                _product = product(instance, *args, **kwargs)
+            else:
+                _product = product(*args, **kwargs)
+        else:
+            _product = product
+
+        if callable(target):
+            if instance is not None:
+                _target = target(instance, *args, **kwargs)
+            else:
+                _target = target(*args, **kwargs)
+        else:
+            _target = target
+
+        if callable(operation):
+            if instance is not None:
+                _operation = operation(instance, *args, **kwargs)
+            else:
+                _operation = operation(*args, **kwargs)
+        else:
+            _operation = operation
+
+        with DatastoreTrace(transaction, _product, _target, _operation):
+            return wrapped(*args, **kwargs)
+
+    return FunctionWrapper(wrapped, _nr_datastore_trace_wrapper_)
+
+
+def datastore_trace(product, target, operation):
+    return functools.partial(DatastoreTraceWrapper, product=product,
+            target=target, operation=operation)
+
+
+def wrap_datastore_trace(module, object_path, product, target, operation):
+    wrap_object(module, object_path, DatastoreTraceWrapper,
+            (product, target, operation))
---
+++ v3/lib/python3.6/site-packages/newrelic/api/memcache_trace.py
@@ -0,0 +1,53 @@
+import functools
+
+from newrelic.api.time_trace import TimeTrace
+from newrelic.api.transaction import current_transaction
+from newrelic.core.memcache_node import MemcacheNode
+from newrelic.common.object_wrapper import FunctionWrapper, wrap_object
+
+
+class MemcacheTrace(TimeTrace):
+
+    node = MemcacheNode
+
+    def __init__(self, transaction, command):
+        super(MemcacheTrace, self).__init__(transaction)
+
+        self.command = command
+
+    def __repr__(self):
+        return '<%s %s>' % (self.__class__.__name__, dict(
+                command=self.command))
+
+    def terminal_node(self):
+        return True
+
+
+def MemcacheTraceWrapper(wrapped, command):
+
+    def _nr_wrapper_memcache_trace_(wrapped, instance, args, kwargs):
+        transaction = current_transaction()
+
+        if transaction is None:
+            return wrapped(*args, **kwargs)
+
+        if callable(command):
+            if instance is not None:
+                _command = command(instance, *args, **kwargs)
+            else:
+                _command = command(*args, **kwargs)
+        else:
+            _command = command
+
+        with MemcacheTrace(transaction, _command):
+            return wrapped(*args, **kwargs)
+
+    return FunctionWrapper(wrapped, _nr_wrapper_memcache_trace_)
+
+
+def memcache_trace(command):
+    return functools.partial(MemcacheTraceWrapper, command=command)
+
+
+def wrap_memcache_trace(module, object_path, command):
+    wrap_object(module, object_path, MemcacheTraceWrapper, (command,))
