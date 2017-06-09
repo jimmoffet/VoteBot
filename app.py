b96b4cfb85d875b313ec3d5293f8e4006f9517b4
jimmoffet <jimmoffet@gmail.com>
2017-06-09 15:01:51 -0400

    make scrape.py more robust to updates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, jsonify
 from twilio.twiml.messaging_response import MessagingResponse
-from scrapetest import scrape
+from scrape import scrape
 import random
 
 app = Flask(__name__)
