e3622ca0657f6f06e62126edbdcd1d85a303e727
jimmoffet <jimmoffet@gmail.com>
2017-06-10 19:03:37 -0400

    add gspread integration

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, jsonify
 from twilio.twiml.messaging_response import MessagingResponse
-from scrape import scrape, ping
+from scrape import scrape, ping, pLayer
 import random
 import threading
 
@@ -27,34 +27,70 @@ def hello_monkey2():
 	return str(resp)
 
 # This should live in a csv and be occasionally unloaded into logs
-callers = {
+callers2 = {
     "+17733541500": "Jimbo",
     "+16172837517": "Naseem",
     "+16178524638": "Nadeem",
-    "+14349067428": "Nick",
+    "+14349067428": "Papa Nic *shudder*",
     "+17123105096": "Vidge Boy",
+    "+14145814507": "CarBar, JimJam loves you! Also"
 }
 
 @app.route("/monkey", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond and greet the caller by name."""
 
-    from_number = request.values.get('From', None)
-    if from_number in callers:
-        message = "Hi " + callers[from_number] + ", I'm MeetingBot. Is it creepy that I know who you are?"
-    else:
-        message = "Hi Beta Tester, thanks for the message!"
+	people = people()
+	test = scrape('http://cambridgema.iqm2.com/Citizens/Detail_LegalNotice.aspx?ID=1008')
+	nextmtg = test[0]
 
-    incoming = request.values.get('Body', None)
-    message = message + ' Your message was ' + '-' + incoming + '- '
-    test = scrape('http://cambridgema.iqm2.com/Citizens/Detail_LegalNotice.aspx?ID=1008')
-    randy = test[random.randint(0,len(test)-10)]
-    preface = "Here's a random upcoming meeting: "
-    message = message + preface + randy['date']+" "+randy['time']+" "+randy['agenda']
-    resp = MessagingResponse()
-    resp.message(message)
+	sheet = pLayer()
 
-    return str(resp)
+	# this is a string
+	incoming = request.values.get('Body', None)
+	from_number = request.values.get('From', None)
+
+	# if we know them, write temp variables for use later
+
+	#record caller, time/date (anything else?) to csv or json, do something if we're already in a convo with them
+
+	#if they're new, don't check for keywords, otherwise see if they're already subscribed or are changing their subscription
+
+	#check to see if they've texted us before and how long its been
+	# if 'next' in incoming:
+	# 	preface = "Sure thing. Here's the next meeting: "
+	# 	meeting = test[0]
+	# 	message = preface + meeting
+	# else:
+	#     from_number = request.values.get('From', None)
+	#     if from_number in people:
+	#         message = "Hi " + callers[from_number][1] + ", I'm the City Council MeetingBot. Is it creepy that I know who you are?"
+	#     else:
+	#         message = "Hi Beta Tester, I'm the City Council MeetingBot."
+
+	if 'next' in incoming:
+	        preface = "Sure thing! Here's the next meeting: "
+	        meeting = nextmtg['date']+" "+nextmtg['time']+" "+nextmtg['agenda']
+	        message = preface + meeting
+	else:
+	    
+	    if from_number in people:
+	        message = "Hi " + people[from_number][1] + ", I'm the City Council MeetingBot. Is it creepy that I know who you are?"
+	    else:
+	        message = "Hi Beta Tester, I'm the City Council MeetingBot."
+	    
+		    # message = message + ' Your message was ' + '-' + incoming + '- '
+		    # test = scrape('http://cambridgema.iqm2.com/Citizens/Detail_LegalNotice.aspx?ID=1008')
+		    # randy = test[random.randint(0,len(test)-10)]
+		    # preface = "Here's a random upcoming meeting: "
+		    # message = message + preface + randy['date']+" "+randy['time']+" "+randy['agenda']
+
+		    message = message + ' ' + 'I only do one thing, but I do it well. For a weekly meeting reminder, say "weekly", for monthly, say "monthly" and to see only the very next meeting, say "next". You can say "stop" or "unsubscribe" at any time.'
+
+	resp = MessagingResponse()
+	resp.message(message)
+
+	return str(resp)
 
 
 if __name__ == "__main__":
--- scrape.py
+++ scrape.py
@@ -3,6 +3,13 @@ import requests
 import requests.exceptions
 import random
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+ 
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
 
 def ping(u):
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -50,11 +57,61 @@ def scrape(u):
 
     return culledList
 
+# get persistent layer as list of lists
+def pLayer():
+    # Find a workbook by name and open the first sheet
+    # Make sure you use the right name here.
+    sheet = client.open("OCPF_testdb").sheet1
+    # Extract and print all of the values
+    list_of_vals = sheet.get_all_values()
+    return list_of_vals
+
+
+def people():
+    sheetList = pLayer()
+    rlen = len(sheetList)
+    clen = len(sheetList[0])
+    callers = {}
+    for row in range(rlen):
+        if row == 0:
+            continue
+        tmp = []
+        for i in range(1,10):
+            tmp.append(sheetList[row][i])
+        callers['+' + sheetList[row][0]] = tmp
+    return callers
+        
+
+# test = scrape('http://cambridgema.iqm2.com/Citizens/Detail_LegalNotice.aspx?ID=1008')
+# randy = test[random.randint(0,len(test)-10)]
+# preface = "Hi test monkey, here's a random meeting: "
+# mess = preface+randy['date']+" "+randy['time']+" "+randy['agenda']
+# print(mess)
+
+
+#sheet = pLayer()
+
+people = people()
 test = scrape('http://cambridgema.iqm2.com/Citizens/Detail_LegalNotice.aspx?ID=1008')
-randy = test[random.randint(0,len(test)-10)]
-preface = "Hi test monkey, here's a random meeting: "
-mess = preface+randy['date']+" "+randy['time']+" "+randy['agenda']
-print(mess)
+nextmtg = test[0]
+
+incoming = 'some words'
+if 'next' in incoming:
+        preface = "Sure thing! Here's the next meeting: "
+        meeting = nextmtg['date']+" "+nextmtg['time']+" "+nextmtg['agenda']
+        message = preface + meeting
+else:
+    from_number = '+17733541500'
+    if from_number in people:
+        message = "Hi " + people[from_number][1] + ", I'm the City Council MeetingBot. Is it creepy that I know who you are?"
+    else:
+        message = "Hi Beta Tester, I'm the City Council MeetingBot."
+
+print(message)
+
+#sheet.update_cell(row, 3, "I just wrote to a spreadsheet using Python!")
+
+print('Done!')
 
 
 
